5e597a896766861edd5b3feff32d3e5bf9c420eb
Carlos Interone <carlos@mostlyclicks.com>
2021-06-29 21:48:44 -0600

    add user item

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+class UserItem  extends Component {
+
+    state = {
+        id: 'id',
+        login: 'mostlyclicks',
+        avatar_url: 'https://avatars.githubusercontent.com/u/26814?v=4',
+        html_url: 'https://github.com/mostlyclicks'
+
+    }
+    
+
+    
+
+
+    render() {
+
+        const { login, avatar_url, html_url } = this.state
+        return (
+            <div className="card text-center">
+                <img src={avatar_url} alt="" className="round-img" style={{width: '60px'}}/>
+                <h3>{login}</h3>
+                <div>
+                    <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default UserItem
