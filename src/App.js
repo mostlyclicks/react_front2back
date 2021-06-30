fe74e6391ea87dfb023748f1334dd0d14a4f7c39
mostlyclicks <carlos@mostlyclicks.com>
2021-06-29 20:14:08 -0600

    section 2 - project 1 start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+
+    const name = "Carlos"
+    const loading = false;
+    const showName = true;
+
+    // if(loading) {
+    //   return <h4>Loading...</h4>
+    // } 
+
+    return (
+      <div className="App">
+
+        {loading ? <h4>Loading... </h4> : <h1>Hello {showName && name} From React</h1>}
+        
+
+      </div>
+    );
+  }
 }
 
 export default App;
