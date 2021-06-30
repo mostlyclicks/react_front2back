8e4b7242149733c57e959efa44cfb15c0b73c7ef
Carlos Interone <carlos@mostlyclicks.com>
2021-06-30 06:10:48 -0600

    add Users

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -2,21 +2,17 @@ import React, { Component } from 'react'
 
 class UserItem  extends Component {
 
-    state = {
-        id: 'id',
-        login: 'mostlyclicks',
-        avatar_url: 'https://avatars.githubusercontent.com/u/26814?v=4',
-        html_url: 'https://github.com/mostlyclicks'
-
-    }
-    
-
-    
-
+    // Now coming from Users props
+    // state = {
+    //     id: 'id',
+    //     login: 'mostlyclicks',
+    //     avatar_url: 'https://avatars.githubusercontent.com/u/26814?v=4',
+    //     html_url: 'https://github.com/mostlyclicks'
+    // }
 
     render() {
 
-        const { login, avatar_url, html_url } = this.state
+        const { login, avatar_url, html_url } = this.props.user
         return (
             <div className="card text-center">
                 <img src={avatar_url} alt="" className="round-img" style={{width: '60px'}}/>
