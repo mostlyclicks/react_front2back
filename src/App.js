6b88c6b9adb84ad994f083faa143f72a803e7806
mostlyclicks <carlos@mostlyclicks.com>
2021-06-29 20:44:29 -0600

    section 3/10

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react'
+import Navbar from './components/layout/Navbar'
 import './App.css';
+
 
 class App extends Component {
   render() {
 
-    const name = "Carlos"
-    const loading = false;
-    const showName = true;
-
-    // if(loading) {
-    //   return <h4>Loading...</h4>
-    // } 
-
     return (
       <div className="App">
-
-        {loading ? <h4>Loading... </h4> : <h1>Hello {showName && name} From React</h1>}
-        
-
+        <Navbar />
       </div>
     );
   }
